3ba36fe44b3dfa6b8c8014e156c0f34fb7001274
FLogan1991 <furlong_logan@columbusstate.edu>
2023-09-24 21:19:54 -0400

    Updated newfeatures.py for Lab 4 assignment

--- newfeature.py
+++ newfeature.py
@@ -5,5 +5,9 @@ def myFunc:
 
     print('We meet again')
 
+    #This is for the github addition
+    print("If you didn't know I wasn't named after the snake" 
+    print("but from Monty Python's Flying Circus")
+
 
 myFunc
